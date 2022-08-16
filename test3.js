3ef21002382a9d463878359faf946dd87e486965
hardman0218 <59394004+hardman0218@users.noreply.github.com>
2022-08-16 15:23:27 +0900

    test

---
+++ test3.js
@@ -0,0 +1,105 @@
+let currentValue=0;            
+let numberString="";            
+let currentLeftValue=0;
+let symbol="";
+let count=0;
+function PushValue(id)
+{
+    numberString+=String(id);
+    currentValue=parseFloat(numberString);
+    let resultForm = document.getElementById("result");
+    resultForm.value = numberString; 
+}
+function Plus()
+{
+    if(currentLeftValue===0){
+        currentLeftValue=currentValue;
+    }else{
+//        console.log(currentLeftValue);        
+        currentLeftValue+=currentValue;
+    }
+    RetentionNumber();
+    symbol="plus";
+}
+function Minus(){
+    if(currentLeftValue===0){
+        currentLeftValue=currentValue;
+    }else{        
+        currentLeftValue-=currentValue;
+    }
+    RetentionNumber();
+    symbol="minus";
+}
+function Multiplication(){
+    if(currentLeftValue===0){
+        currentLeftValue=currentValue;
+    }else{        
+        currentLeftValue*=currentValue;
+    }
+    RetentionNumber();
+    symbol="multiplication";
+}
+function Division(){
+    if(currentLeftValue===0){
+        currentLeftValue=currentValue;
+        console.log("a");
+    }else{        
+        console.log(currentLeftValue);
+        currentLeftValue/=currentValue;
+    }
+    RetentionNumber();
+    symbol="division";
+}
+function PlusMinusKey(){
+    currentValue=-currentValue;
+    let resultForm = document.getElementById("result");
+    resultForm.value = currentValue; 
+}
+function RetentionNumber(){
+    //if(currentLeftValue===0){
+      //  currentLeftValue+=currentValue;
+    //}
+    numberString="";
+    currentValue=0;
+}
+function DecimalPoint(){
+    numberString+=".";
+    let resultForm = document.getElementById("result");
+    resultForm.value = numberString; 
+}
+function AllClearKey(){
+    numberString="";
+    currentValue=0;
+    symbol="";
+    currentLeftValue=0;
+    let resultForm = document.getElementById("result");
+    resultForm.value = numberString; 
+}
+function ClearEntryKey(){
+    numberString="";
+    currentValue=0;
+    let resultForm = document.getElementById("result");
+    resultForm.value = numberString; 
+}       
+function Equal()
+{
+    switch(symbol){
+    case "plus":
+        currentLeftValue+=currentValue;
+        break;
+        case "minus":
+        currentLeftValue-=currentValue;
+        break;
+        case "multiplication":
+        currentLeftValue*=currentValue;
+        break;
+        case "division":
+        currentLeftValue/=currentValue;
+        break;
+    }
+    numberString="";
+    currentValue=0;
+    symbol="";
+    let resultForm = document.getElementById("result");
+    resultForm.value = currentLeftValue; 
+}
